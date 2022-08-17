f0775fe2efe84c83dfc94bab6ff282483c9ad1fb
Zauram <zauram@code.edu.az>
2022-08-17 18:47:35 +0400

    Update 4

--- Assets/Javascript/main.js
+++ Assets/Javascript/main.js
@@ -2,6 +2,7 @@ let input1=document.querySelector("#price");
 let input2=document.querySelector("#month");
 let input3=document.querySelector("#percentage");
 let button1=document.querySelector("#calculate");
+let button2=document.querySelector("#clean");
 let result=document.querySelector(".list-group");
 let finalAmount=document.querySelector("#finalAmounts");
 
@@ -11,10 +12,19 @@ button1.addEventListener("click", function(){
         return;
     }
 
-
+    result.innerHTML="";
+    finalAmount.innerHTML="";
     for(let i=0; i<input2.value; i++){
-    result.innerHTML += "<li class='list-group-item'>"+input1.value/input2.value*(1+input3.value/100)+"</li>";
+    result.innerHTML += "<li class='list-group-item'>"+(input1.value*(1+input3.value*input2.value/100)/input2.value)+"</li>";
     }
 
-    finalAmount.innerHTML += "Toplam: " + input1.value*(1+input3.value/100);
+    finalAmount.innerHTML += "Toplam: " + (input1.value*(1+input3.value*input2.value/100));
 });
+
+button2.addEventListener("click", function(){
+    result.innerHTML="";
+    finalAmount.innerHTML="";
+    input1.value="";
+    input2.value="";
+    input3.value="";
+})
