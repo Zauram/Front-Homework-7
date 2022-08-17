1e007a34611900e310016d6e601725db1f7701a3
Zauram <zauram@code.edu.az>
2022-08-17 10:17:18 +0400

    Update 3

--- Assets/Javascript/main.js
+++ Assets/Javascript/main.js
@@ -3,6 +3,8 @@ let input2=document.querySelector("#month");
 let input3=document.querySelector("#percentage");
 let button1=document.querySelector("#calculate");
 let result=document.querySelector(".list-group");
+let finalAmount=document.querySelector("#finalAmounts");
+
 button1.addEventListener("click", function(){
     if(input1.value=="" || input2.value=="" || input3.value==""){
         alert("Fill the inputs!");
@@ -13,4 +15,6 @@ button1.addEventListener("click", function(){
     for(let i=0; i<input2.value; i++){
     result.innerHTML += "<li class='list-group-item'>"+input1.value/input2.value*(1+input3.value/100)+"</li>";
     }
+
+    finalAmount.innerHTML += "Toplam: " + input1.value*(1+input3.value/100);
 });
